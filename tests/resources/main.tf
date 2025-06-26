0742f15c168e441295e8b00b6b4dd127c9780a31
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-06-26 08:09:18 +0100

    chore(deps): update terraform terraform-ibm-modules/base-ocp-vpc/ibm to v3.50.2 (#113)
    Co-authored-by: Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -62,7 +62,7 @@ locals {
 
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.50.0"
+  version              = "3.50.2"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
