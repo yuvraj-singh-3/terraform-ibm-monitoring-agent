73af6620bc6821977d0e62bbe44b7bb2d6eed224
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-07-15 20:09:20 +0100

    fix(deps): update helm release sysdig-deploy to latest (#137)

--- variables.tf
+++ variables.tf
@@ -183,7 +183,7 @@ variable "chart_location" {
 variable "chart_version" {
   description = "The version of the Cloud Monitoring agent helm chart to deploy."
   type        = string
-  default     = "1.89.0" # registryUrl: charts.sysdig.com
+  default     = "1.89.1" # registryUrl: charts.sysdig.com
   nullable    = false
 }
 
