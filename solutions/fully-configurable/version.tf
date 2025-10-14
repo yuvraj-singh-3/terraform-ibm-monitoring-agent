be679ea400083b8e432244042ce95207725df37b
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-14 07:02:24 +0100

    fix(deps): update dependencies (#227)

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -6,7 +6,7 @@ terraform {
     # Lock DA into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.83.3"
+      version = "1.84.0"
     }
     helm = {
       source  = "hashicorp/helm"
